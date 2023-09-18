f910daf4630e548cf8de923059ad36acff866861
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-09-18 14:15:10 -0300

    notebook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+
+import Home from './componentes/pages/Home';
+import Contact from './componentes/pages/Contact';
+import Company from './componentes/pages/Company';
+import NewProject from './componentes/pages/NewProject';
+
+import NavBar from './componentes/NavBar/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-
-      <p>Costs</p>
-
-    </div>
+    return (
+        <Router>
+            <ul>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+            <li>
+                <Link to='/contact'>Contato</Link>
+            </li>
+            <li>
+                <Link to='/company'>Sobre</Link>
+            </li>
+            <li>
+                <Link to='/newproject'>Novo Projeto</Link>
+            </li>
+        </ul>
+            
+            <Routes>
+                <Route exact path='/' element={<Home/>}/>
+                <Route  path='/company' element={<Company/>}/>
+                <Route  path='/contact' element={<Contact/>}/>
+                <Route  path='/newproject' element={<NewProject/>}/>
+            </Routes>
+        </Router>
   );
 }
 
