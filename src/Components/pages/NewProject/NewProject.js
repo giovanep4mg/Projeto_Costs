f730af0eda3dc5a4f84de64c8157c6d29f50dca3
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-09-19 10:05:38 -0300

    aula 21 pag para formulario de projeto

--- src/Components/pages/NewProject/NewProject.js
+++ src/Components/pages/NewProject/NewProject.js
@@ -1,8 +1,13 @@
+import ProjectForm from '../../Project/ProjectForm/ProjectForm';
+import styles from './NewProject.module.css';
+
 function NewProject(){
     return(
-        <>
-            <h1>NewProject</h1>
-        </>
+        <div className={styles.newproject_container}>
+            <h1>Criar projeto</h1>
+            <p>Crie seu projeto pra depois adicionar serviços.</p>
+            <ProjectForm/>
+        </div>
     )
 }
 export default NewProject;
