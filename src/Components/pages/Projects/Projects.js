1c59a623aea3d33c4999b83ec9124904e3d5d74a
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-09-25 17:06:37 -0300

    aula26 criando dashboard

--- src/Components/pages/Projects/Projects.js
+++ src/Components/pages/Projects/Projects.js
@@ -1,6 +1,12 @@
 import Message from "../../Layout/Message/Message";
 
+import styles from './Projects.module.css';
+
+import Container from '../../Layout/Container/Container';
+
 import { useLocation} from 'react-router-dom';
+
+import LinkButton from '../../Layout/LinkButton/LinkButton';
 
 function Projects(){
     
@@ -12,9 +18,15 @@ function Projects(){
     console.log("mensagem "+message)
     
     return(
-        <div>
-            <h1>Meus projetos!</h1>
+        <div className={styles.projects_container} >
+            <div className={styles.title_container} >
+                <h1>Meus projetos!</h1>
+                <LinkButton to="/newproject" text="Criar projeto" />
+            </div>
             {message && <Message type="sucess" msg={message}/> }
+            <Container customClass="start">
+                <p>projetos</p>
+            </Container>
         </div>
     )
 }
