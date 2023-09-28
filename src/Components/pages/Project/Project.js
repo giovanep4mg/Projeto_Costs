598d2bcd1bb01649e5d06d5e1d9eb85f3cce9d1b
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-09-28 14:44:47 -0300

    aula33 criando area de servicos

--- src/Components/pages/Project/Project.js
+++ src/Components/pages/Project/Project.js
@@ -14,6 +14,7 @@ function Project(){
     const {id} = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -36,6 +37,8 @@ function Project(){
     }, [id])
 
     function editPost(project){
+        setMessage('')
+
         //budget validation
         if(project.budget < project.cost){
             // mensagem
@@ -64,6 +67,10 @@ function Project(){
 
     function toggleProjectForm(){
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm(){
+        setShowServiceForm(!showServiceForm)
     }
 
     return(
@@ -102,6 +109,23 @@ function Project(){
                                 </div>
                             )}
                         </div>
+                        <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button onClick={toggleServiceForm} className={styles.btn}>
+                                {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                            </button>
+                            <div className={styles.project_info}>
+                                {showServiceForm && (
+                                    <div>
+                                        Formulário do serviço
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                        <h2>Serviços</h2>
+                        <Container customClass="start">
+                            <p>Itens de serviços</p>
+                        </Container>
                     </Container>
                 </div>
             )
