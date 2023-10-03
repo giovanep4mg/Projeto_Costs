dea7acb28bb1e0f71656122daf0dc1370ae76873
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-10-03 17:09:51 -0300

    projeto finalizado

--- src/Components/pages/Company/Company.js
+++ src/Components/pages/Company/Company.js
@@ -1,10 +1,29 @@
+import styles from './Company.module.css';
+
+
 function Company(){
     return(
-        <>
-            <h1>Company
-            
-            </h1>
-        </>
+        <div className={styles.company_tudo}>
+            <div>
+                <h1>
+                    Projeto criado assistindo ao Curso de React
+                </h1>
+            </div>
+            <div>
+                <h2>
+                    Curso de React, do canal <span>Matheus</span> <span>Battisti</span>.
+                </h2>
+            </div>
+            <div>
+                <p>
+                    <a href="https://youtube.com/playlist?list=PLnDvRpP8BneyVA0SZ2okm-QBojomniQVO&si=aGOQI2TcQAGR1_KQ">
+                        Link da playlist
+                    </a>
+                </p>
+            </div>
+        </div>
+        
+        
     )
 }
 export default Company;
