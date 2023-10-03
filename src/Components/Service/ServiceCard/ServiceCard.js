f5a8783afdfedcc924bc3c4f1fbc1e6221588c0a
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-10-03 16:23:39 -0300

    aula36 removendo servicos finalizado

--- src/Components/Service/ServiceCard/ServiceCard.js
+++ src/Components/Service/ServiceCard/ServiceCard.js
@@ -4,7 +4,8 @@ import {BsFillTrashFill} from 'react-icons/bs';
 function ServiceCard({id, name, description, cost, handleRemove}){
 
     const remove = (e) => {
-
+        e.preventDefault()
+        handleRemove(id, cost)
     }
 
     return(
